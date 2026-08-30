1ff3e5f63a4b160c5370770cc43d4cc5a1a790a8
unknown <srikanthram87@gmail.com>
2026-08-30 07:51:19 +0530

    oil&karam

--- main.tf
+++ main.tf
@@ -39,7 +39,7 @@ resource "terraform_data" "main" {
   }
 }
  
-/* resource "aws_ec2_instance_state" "main" {   
+ resource "aws_ec2_instance_state" "main" {   
   instance_id = aws_instance.main.id
   state       = "stopped"
   depends_on = [terraform_data.main]   #its for stopping the instance after completing of terraform data
